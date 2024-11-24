72ccd0a85bd6e2d581203c92955604740aa8bc31
zafer_sibi <sivaprakasams267@gmail.com>
2024-11-24 10:49:58 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        bannerImage1:"url('./images/banner-1.jpg')",
+        bannerImage2:"url('./images/banner-2.jpg')",
+        bannerImage3:"url('./images/banner-3.jpg')",
+        blackOverlay:"linear-gradient(to bottom,rgba(0,0,0,0) 0%,rgba(0,0,0,0.8) 100%)"
+      },
+      colors: {
+        'overlay-blue': '#002e5b',
+      },
+    },
+  },
+  plugins: [],
+}
+
